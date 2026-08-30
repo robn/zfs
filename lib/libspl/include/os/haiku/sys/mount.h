1cc6a7e2f8b08510077b7e2bb1b71f4ed1432d45
Rob Norris <robn@despairlabs.com>
2026-08-30 20:26:32 +1000

    haiku: hack around umount2

--- lib/libspl/include/os/haiku/sys/mount.h
+++ lib/libspl/include/os/haiku/sys/mount.h
@@ -1,6 +1,7 @@
 /*
  * HAIKU PORTING NOTES:
  * - bare minimum to make it build. I have no idea what mounting looks like yet.
+ * - umount2 come on
  */
 
 #ifndef _LIBSPL_SYS_MOUNT_H
@@ -9,5 +10,11 @@
 #define	MS_FORCE	(1<<0)
 #define	MS_CRYPT	(1<<1)
 #define	MS_OVERLAY	(1<<2)
+
+/*
+ * hack, zfs_main calls umount2() direct, not through an OS layer. I'm
+ * unreasonably upset about this.
+ */
+#define	umount2(p, f)	((void) p, (void) f, 0)
 
 #endif /* _LIBSPL_SYS_MOUNT_H */
